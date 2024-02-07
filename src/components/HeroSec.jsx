fdbe6931fdd617d4812adc091cbf6426f76057ba
Abdul-Hafeez5 <hafeezrahimoon531@gmail.com>
2024-02-07 17:33:06 +0500

    complete HeroSec

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -12,9 +12,9 @@ const HeroSec = () => {
       <Swiper navigation={true} modules={[Navigation]} className="">
         <SwiperSlide>
           {/* <div className={`flex justify-center text-center w-full`}> */}
-          <div className="background-container px-16 py-10 pr-[700px]">
-            <h1 className="text-[64px] font-bold font-mono ">
-              Don't miss amazing grocerry details
+          <div className="background-container rounded-xl px-16 py-10 pr-[700px]">
+            <h1 className="text-[64px] leading-[72px] font-bold font-mono ">
+              Fresh vegetables big discount
             </h1>
             <p className="text-3xl text-[#7E7E7E] my-2">
               SignUp for daily newsLetter
@@ -29,7 +29,21 @@ const HeroSec = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="bg-container"></div>
+          <div className="bg-container rounded-xl px-20 py-10 pr-[700px]">
+            <h1 className="text-[64px] leading-[72px] font-bold font-mono ">
+              Don't miss amazing grocerry details
+            </h1>
+            <p className="text-3xl text-[#7E7E7E] my-2">
+              SignUp for daily newsLetter
+            </p>
+            <div className=" inline-flex items-center mt-8 bg-white rounded-3xl pl-4  mr-2">
+              <FiSend />
+              <input type="text" className=" p-3 mr-2 border-none" />
+              <button className="bg-[#3bb77e] hover:bg-[#47c98c] rounded-3xl py-3 px-6 text-white">
+                Subscribe
+              </button>
+            </div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
