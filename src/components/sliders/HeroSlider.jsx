8c82effdc990f62cfb3748a29858b79da9006513
Abdul-Hafeez5 <hafeezrahimoon531@gmail.com>
2024-02-09 19:48:26 +0500

    Complete HeroSec with autoplay

--- src/components/sliders/HeroSlider.jsx
+++ src/components/sliders/HeroSlider.jsx
@@ -1,51 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { LiaLessThanSolid } from "react-icons/lia";
 import { LiaGreaterThanSolid } from "react-icons/lia";
 import { slider_1, slider_2 } from "../../assets/HeroSlider/index";
 import HeroSec from "../HeroSec";
+// import "./HeroSlider.css"
 const HeroSlider = () => {
+  const prevButtonRef = useRef(null);
+  const nextButtonRef = useRef(null);
   return (
-    <Swiper
-      className="mt-10 lg:px-10"
-      navigation={{
-        prevEl: ".swiper-button-prev ",
-        nextEl: ".swiper-button-next",
-      }}
-      modules={[Navigation, Pagination]}
-      pagination={{ clickable: true }}
-    >
-      <SwiperSlide>
-        <div className=" swiper-button-prev">
-          <LiaLessThanSolid />
-        </div>
-        <HeroSec
-          img={slider_1}
-          heading={"Don't miss amazing grocerry deals"}
-          subHeading={"sign up for daily newsletter"}
-        />
-        <div className="swiper-button-next">
-          <LiaGreaterThanSolid />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className=" swiper-button-prev">
-          <LiaLessThanSolid />
-        </div>
-        <HeroSec
-          img={slider_2}
-          heading={"Fresh vegetables Big discounts"}
-          subHeading={"Save up to 50% off on your first order"}
-        />
-        <div className="swiper-button-next">
-          <LiaGreaterThanSolid />
-        </div>
-      </SwiperSlide>
-    </Swiper>
+    <div className="relative">
+      <div className="ml-6 top-2/3 swiper-button-prev" ref={prevButtonRef}>
+        <LiaLessThanSolid />
+      </div>
+      <div
+        className="absolute mr-6 transform -translate-y-1/2 top-1/2 right-4 swiper-button-next"
+        ref={nextButtonRef}
+      >
+        <LiaGreaterThanSolid />
+      </div>
+      <Swiper
+        className="mt-10 lg:px-10"
+        navigation={{
+          prevEl: prevButtonRef.current,
+          nextEl: nextButtonRef.current,
+        }}
+        modules={[Navigation, Pagination, Autoplay]}
+        // autoplay={{ delay: 5000 }}
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide>
+          <HeroSec
+            img={slider_1}
+            heading={"Don't miss amazing grocerry deals"}
+            subHeading={"sign up for daily newsletter"}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <HeroSec
+            img={slider_2}
+            heading={"Fresh vegetables Big discounts"}
+            subHeading={"Save up to 50% off on your first order"}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 
