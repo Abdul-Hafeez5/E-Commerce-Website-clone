0e82715a5fda27626934b3c8169525363dea046a
Abdul-Hafeez5 <hafeezrahimoon531@gmail.com>
2024-02-10 14:52:01 +0500

    completed product section

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,27 +2,36 @@ import React from "react";
 import { BsCart } from "react-icons/bs";
 import { product_1_1 } from "../assets/Products/index";
 
-const Product = ({ sale, img, type, heading, seller, price, discount }) => {
+const Product = ({
+  sale,
+  img,
+  hovImg,
+  type,
+  heading,
+  seller,
+  price,
+  discount,
+}) => {
   return (
-    <div className="flex flex-col w-[350px] border rounded-3xl">
-      <h1 className="w-16 px-4 py-2 text-white font-lato text-[12px] bg-cyan-300 rounded-br-3xl">
-        Hello
+    <div className="flex flex-col border max-w-72 rounded-3xl">
+      <h1 className="w-16 px-4 py-2 text-white font-lato text-[12px] bg-[#f74b81] rounded-br-3xl">
+        {sale}
       </h1>
-      <div className="w-72 h-72">
-        <img src={product_1_1} alt="" />
+      <div className="justify-center w-60 h-60 xl:w-64 xl:h-64">
+        <img src={img} alt="products" />
       </div>
       <div className="mx-8 my-8 ">
-        <span className="font-lato text-secondary  text-[12px]">snack</span>
-        <h1 className="my-2 text-base font-bold font-quickSand">Heading</h1>
+        <span className="font-lato text-secondary  text-[12px]">{type}</span>
+        <h1 className="my-2 text-base font-bold font-quickSand">{heading}</h1>
         <p>star and rating</p>
         <p className="text-sm font-lato text-secondary">
-          By <span className=" text-primary">Hafeez</span>
+          By <span className=" text-primary">{seller}</span>
         </p>
         <div className="flex items-center justify-between mt-3 ">
           <div className="font-bold font-quickSand">
-            <p className="text-[18px]  text-primary ">$17.85</p>
+            <p className="text-[18px]  text-primary ">${price}</p>
             <p className="relative text-sm text-secondary">
-              $32.8
+              ${discount}
               <span className="absolute left-0 w-9 bottom-2 h-[1.5px] bg-secondary"></span>
             </p>
           </div>
--- src/containers/Products.jsx
+++ src/containers/Products.jsx
@@ -1,8 +1,130 @@
 import React from "react";
 import Product from "../components/Product";
-// import {product_1_1, product_1_2, product_2_1, product_2_2,product_3_1,product_3_2,product_4_1,product_4_2,product_5_1,product_5_2,product_6_1,product_6_2,product_7_1,product_7_2,product_8_1,product_8_2,product_9_1,product_9_2,product_10_1,product_10_2} from "../assets/Products/index"
+import {
+  product_1_1,
+  product_1_2,
+  product_2_1,
+  product_2_2,
+  product_3_1,
+  product_3_2,
+  product_4_1,
+  product_4_2,
+  product_5_1,
+  product_5_2,
+  product_6_1,
+  product_6_2,
+  product_7_1,
+  product_7_2,
+  product_8_1,
+  product_8_2,
+  product_9_1,
+  product_9_2,
+  product_10_1,
+  product_10_2,
+} from "../assets/Products/index";
 
-let productList = [{sale:"Hot", img: "iage1", type:"snack", heading: "h1", seller: "seller", price: 43, discount:50}]
+let productList = [
+  {
+    sale: "Hot",
+    img: product_1_1,
+    hovImg: product_1_2,
+    type: "snack",
+    heading: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+    seller: "NestFood",
+    price: 28.85,
+    discount: 32.8,
+  },
+  {
+    sale: "sale",
+    img: product_2_1,
+    hovImg: product_2_2,
+    type: "Hodo Foods",
+    heading: "All Natural Italian-Style Chicken Meatballs",
+    seller: "Stouffer",
+    price: 52.85,
+    discount: 55.8,
+  },
+  {
+    sale: "New",
+    img: product_3_1,
+    hovImg: product_3_2,
+    type: "snack",
+    heading: "Angie’s Boomchickapop Sweet & Salty Kettle Corn",
+    seller: "StarKist",
+    price: 48.85,
+    discount: 52.8,
+  },
+  {
+    sale: "Hot",
+    img: product_4_1,
+    hovImg: product_4_2,
+    type: "Vegetables",
+    heading: "Foster Farms Takeout Crispy Classic Buffalo Wings",
+    seller: "NestFood",
+    price: 17.85,
+    discount: 19.8,
+  },
+  {
+    sale: "Hot",
+    img: product_5_1,
+    hovImg: product_5_2,
+    type: "snack",
+    heading: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+    seller: "NestFood",
+    price: 28.85,
+    discount: 32.8,
+  },
+  {
+    sale: "Hot",
+    img: product_6_1,
+    hovImg: product_6_2,
+    type: "snack",
+    heading: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+    seller: "NestFood",
+    price: 28.85,
+    discount: 32.8,
+  },
+  {
+    sale: "Hot",
+    img: product_7_1,
+    hovImg: product_7_2,
+    type: "snack",
+    heading: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+    seller: "NestFood",
+    price: 28.85,
+    discount: 32.8,
+  },
+  {
+    sale: "Hot",
+    img: product_8_1,
+    hovImg: product_8_2,
+    type: "snack",
+    heading: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+    seller: "NestFood",
+    price: 28.85,
+    discount: 32.8,
+  },
+  {
+    sale: "Hot",
+    img: product_9_1,
+    hovImg: product_9_2,
+    type: "snack",
+    heading: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+    seller: "NestFood",
+    price: 28.85,
+    discount: 32.8,
+  },
+  {
+    sale: "Hot",
+    img: product_10_1,
+    hovImg: product_10_2,
+    type: "snack",
+    heading: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+    seller: "NestFood",
+    price: 28.85,
+    discount: 32.8,
+  },
+];
 
 const Products = () => {
   return (
@@ -22,8 +144,19 @@ const Products = () => {
         </div>
       </div>
       <div className="flex mx-6 gap-x-6">
-        <Product />
-        <Product />
+        {productList.map((item, index) => (
+          <Product
+            key={index}
+            sale={item.sale}
+            img={item.img}
+            hovImg={item.hovImg}
+            type={item.type}
+            heading={item.heading}
+            seller={item.seller}
+            price={item.price}
+            discount={item.discount}
+          />
+        ))}
       </div>
     </div>
   );
