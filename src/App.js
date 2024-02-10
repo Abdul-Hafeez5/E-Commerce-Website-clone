1393a24623868ca1f648773d462ab2e1f6c02b5a
Abdul-Hafeez5 <hafeezrahimoon531@gmail.com>
2024-02-10 11:34:31 +0500

    Completed Banner Section

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <HeroSlider />
       <Categories />
-      {/* <Banners /> */}
+      <Banners />
     </div>
   );
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,17 +3,15 @@ import { FaArrowRight } from "react-icons/fa";
 
 const Banner = ({ title, img }) => {
   return (
-    <div className="w-64 h-64">
-      <div
-        className={`flex  font-QuickSand w-[500px] h-64 bg-cover bg-center  rounded-lg pt-20 pr-60`}
-        style={{ backgroundImage: `url(${img})` }}
-      >
-        <div className="ml-8">
-          <h1 className="text-[18px] font-bold mb-5">{title}</h1>
-          <div className="inline-flex items-center justify-center gap-x-2 py-2 px-4 text-white bg-[#3bb77e] rounded-md">
-            <button className="font-bold">Shop Now</button>
-            <FaArrowRight />
-          </div>
+    <div
+      className={`flex items-center justify-center font-QuickSand h-64 mb-8 bg-cover bg-center pr-32 rounded-lg xs:pr-40  sm:pr-60 md:pr-10 xl:pr-44`}
+      style={{ backgroundImage: `url(${img})` }}
+    >
+      <div className="ml-8">
+        <h1 className="text-[18px] font-bold mb-5">{title}</h1>
+        <div className="inline-flex items-center sm:justify-center gap-x-2 py-2 xs:px-4 text-white bg-[#3bb77e] rounded-md">
+          <button className="xs:font-bold text-[12px]">Shop Now</button>
+          <FaArrowRight />
         </div>
       </div>
     </div>
